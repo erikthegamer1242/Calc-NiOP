9725525e46106a80c24db56223bb192aa83387c9
JozDe <erikbraun1242@gmail.com>
2023-03-06 14:44:14 +0100

    .

--- Calc/MainPage.xaml.cs
+++ Calc/MainPage.xaml.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.ServiceModel.Channels;
+using System.Text.RegularExpressions;
+using Windows.Devices.Power;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -23,8 +26,10 @@ namespace Calc
         public MainPage()
         {
             this.InitializeComponent();
+            textBlockAns.Text = " ";
         }
-
+        int baza = 10;
+        
         private void button1_Click(object sender, RoutedEventArgs e)
         {
             textBlockAns.Text = Convert.ToString(int.Parse(textBox1.Text) + int.Parse(textBox2.Text));
@@ -42,8 +47,62 @@ namespace Calc
 
         private void button4_Click(object sender, RoutedEventArgs e)
         {
-            textBlockAns.Text = Convert.ToString(int.Parse(textBox1.Text) / int.Parse(textBox2.Text));
+            textBlockAns.Text = Convert.ToString(double.Parse(textBox1.Text) / double.Parse(textBox2.Text));
+        }
+
+        private void Octal_Checked(object sender, RoutedEventArgs e)
+        {
+            textBlockAns.Text = Convert.ToString(Convert.ToInt32(textBlockAns.Text, baza), 8);
+            baza = 8;
+        }
+
+        private void binary_Checked(object sender, RoutedEventArgs e)
+        {
+            textBlockAns.Text = Convert.ToString(Convert.ToInt32(textBlockAns.Text, baza), 2);
+            baza = 2;
+        }
+
+        private void hex_Checked(object sender, RoutedEventArgs e)
+        {
+            textBlockAns.Text = Convert.ToString(Convert.ToInt32(textBlockAns.Text, baza), 16);
+            baza= 16;
+        }
+
+        private void _decimal_Checked(object sender, RoutedEventArgs e)
+        {
+            textBlockAns.Text = Convert.ToString(Convert.ToInt32(textBlockAns.Text, baza), 10);
+            baza = 10;
+        }
+
+        private void textBox1_TextChanging(TextBox sender, TextBoxTextChangingEventArgs args)
+        {
+            string pattern = "[^0-9]";
+            switch(baza)
+            {
+                case (2):
+                    pattern = "[^0-1]";
+                    break;
+                case (8):
+                    pattern = "[^0-7]";
+                    break;
+                case (10):
+                    pattern = "[^0-9]";
+                    break;
+                case (16):
+                    pattern = "[^0-F]";
+                    break;
+
+            }
+            if (Regex.IsMatch(textBox1.Text, pattern))
+            {
+                textBox1.Text = textBox1.Text.Remove(textBox1.Text.Length - 1);
+            }
+        }
+
+        private void textBox1_TextChanging(object sender, TextChangedEventArgs e)
+        {
+
         }
     }
 
-}
+} 
